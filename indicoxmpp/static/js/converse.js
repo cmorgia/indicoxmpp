a4bafb79356e26fbdad5eafb696782633a5c4973
Claudio Morgia <cmorgia@gmail.com>
2015-12-01 12:52:50 +0100

    Fixed typeahead version conflict

--- indicoxmpp/static/js/converse.js
+++ indicoxmpp/static/js/converse.js
@@ -1,14 +1,6 @@
 require(['converse'], function (converse) {
     $(function () {
         if (currentUser!='') {
-            $(window).scroll(function () {
-                if ($(".navbar").offset().top > 50) {
-                    $(".navbar-fixed-top").addClass("top-nav-collapse");
-                } else {
-                    $(".navbar-fixed-top").removeClass("top-nav-collapse");
-                }
-            });
-            //jQuery for page scrolling feature - requires jQuery Easing plugin
             $('.page-scroll a').bind('click', function (event) {
                 var $anchor = $(this);
                 $('html, body').stop().animate({
